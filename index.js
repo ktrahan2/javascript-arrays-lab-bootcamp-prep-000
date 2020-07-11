b5e34c72b0dc1c4250148d5ea0250471402a1504
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-11 12:24:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function prependKitten(name) {
   var kittens5 = kittens
 }
 function removeLastKitten() {
-  var kittens3 = kittens.slice(1, 2)
+  var kittens3 = kittens.slice(0, 2)
   return kittens3
 }
 function removeFirstKitten() {
