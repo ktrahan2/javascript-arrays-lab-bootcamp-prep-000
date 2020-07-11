a4dd5aedcceae5ed2560e86b5d16ff1677ac039c
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-11 12:10:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 function destructivelyAppendKitten(array, element) {
   array.push(element)
 }
+function destructivelyPrependKitten(array, element) {
+  array.unshift(element)
+  return array
+}
